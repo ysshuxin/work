c0298bdec5da2c27c18a78fb09cbe9261dde8bca
ysshuxin <ysshuxin@outlook.com>
2019-01-22 14:06:25 +0800

    01.22

--- antddemo/src/Component/uploadItem/Login.js
+++ antddemo/src/Component/uploadItem/Login.js
@@ -2,13 +2,13 @@ import React, { Component } from "react";
 import Nav from "../Nav";
 import Foot from "../Foot";
 import Logininf from './Logininf' 
-import F from '../../context/f'
+
 class Login extends Component {
   render() {
     return (
       <div>
       <Nav></Nav>
-      <F></F>
+    
       <div style={{width:"100%",position:"absolute",top:"65px",bottom:"65px",overflow:"auto"}} id="S1index"> 
       <Logininf></Logininf>
       </div>
